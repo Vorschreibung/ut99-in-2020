9374e7d1de156adca29180f744ea63c47d079662
Vorschreibung <Vorschreibung@gmail.com>
2020-09-05 23:11:22 +0200

    smurtbuild(tm) 4.0

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 die() {
     printf '%s\n' "$1" >&2
     exit 1
 }
 
+outputHtmls=()
 compile_md() {
     inputf="$1.md"
     outputf="$1.html"
     vorsch-md2html "$inputf" > "$outputf" || die "failed to compile $inputf"
-    git add "$outputf" || die "git: failed to add $outputf"
+    outputHtmls+=("$outputf")
 }
+
+compile_md "./index"
+
 
 if ! git show-ref "refs/heads/gh-pages" >/dev/null; then
     git checkout --orphan "gh-pages" || die "failed to create & switch to gh-pages"
@@ -18,5 +22,4 @@ else
     git checkout "gh-pages"  || die "failed to switch to gh-pages"
 fi
 
-
-compile_md "./index"
+git add "${outputHtmls[@]}" || die "git: failed to add ${outputHtmls[*]}"
