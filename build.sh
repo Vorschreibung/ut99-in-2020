51dcb91dec88ee935392563be6b2863ee03ccaca
Vorschreibung <Vorschreibung@gmail.com>
2020-09-06 20:26:24 +0200

    smurtbuild(tm) 5.0

--- build.sh
+++ build.sh
@@ -4,6 +4,30 @@ die() {
     exit 1
 }
 
+usage() {
+    echo "usage: [-c|--commit]"
+    exit 1
+}
+
+commit=
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            usage; exit ;;
+        -c|--commit)
+            preview=1
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *) break ;;
+    esac
+
+    shift
+done
+
+
 outputHtmls=()
 compile_md() {
     inputf="$1.md"
@@ -12,14 +36,17 @@ compile_md() {
     outputHtmls+=("$outputf")
 }
 
+autoversion "./dl/ut99-in-2020-check.bat" "./dl/ut99-install-umod.bat"
 compile_md "./index"
+compile_md "./checksums"
 
+if [[ $commit ]]; then
+    if ! git show-ref "refs/heads/gh-pages" >/dev/null; then
+        git checkout --orphan "gh-pages" || die "failed to create & switch to gh-pages"
+        git reset
+    else
+        git checkout "gh-pages"  || die "failed to switch to gh-pages"
+    fi
 
-if ! git show-ref "refs/heads/gh-pages" >/dev/null; then
-    git checkout --orphan "gh-pages" || die "failed to create & switch to gh-pages"
-    git reset
-else
-    git checkout "gh-pages"  || die "failed to switch to gh-pages"
+    git add "${outputHtmls[@]}" || die "git: failed to add ${outputHtmls[*]}"
 fi
-
-git add "${outputHtmls[@]}" || die "git: failed to add ${outputHtmls[*]}"
