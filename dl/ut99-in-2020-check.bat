5d7171fc26a159cae56f0df27e2755218771a422
Vorschreibung <Vorschreibung@gmail.com>
2020-09-07 01:21:15 +0200

    ut99-in-2020-check: remove obsolete TODO

--- dl/ut99-in-2020-check.bat
+++ dl/ut99-in-2020-check.bat
@@ -106,7 +106,6 @@ FOR /F "usebackq tokens=*" %%A IN ("./System/UnrealTournament.ini") DO (
 
         REM R8. directinput
         IF "!section!"=="WinDrv.WindowsClient" IF "!key!"=="UseDirectInput" IF NOT "!val!"=="True" (
-            REM @TODO check if ddinput dll exists
             ECHO [ERROR] You have disabled Direct Input. This leads to buggy mouse aim.
             ECHO ^( see https://vorschreibung.github.io/ut99-in-2020/#use-ut99dinput ^)
             ECHO.
